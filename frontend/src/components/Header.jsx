9abcd25188aa8d9e7076266556cc56ba94018313
jcsodenkamp <jcsodenkamp@gmail.com>
2022-12-08 14:03:16 -0700

    front and backend in working state

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Navbar, Nav, Container, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+function Header() {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container fluid>
+                    <LinkContainer to="/">
+                        <Navbar.Brand >BRAINSTORM BRAND IDEAS</Navbar.Brand>
+                    </LinkContainer>
+
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav className="mr-auto">
+
+                            <LinkContainer to="/cart">
+                                <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                            </LinkContainer>
+
+                            <LinkContainer to="/login">
+                                <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
+                            </LinkContainer>
+                            
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+}
+
+export default Header;
---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Products from "../components/Product"
+import axios from "axios";
+
+
+
+
+
+function HomeScreen() {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        async function fetchProducts() {
+            
+            const { data } = await axios.get('http://127.0.0.1:8000/api/products/')
+            setProducts(data)
+        }
+
+        fetchProducts()
+        
+    },[])
+
+    return (
+        <div>
+            <h1>Lastest Products</h1>
+            <Row>
+                {products.map((product) => (
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Products product={product}/>
+                    </Col>
+                ))}
+            </Row>
+        </div>
+    );
+}
+
+export default HomeScreen;
